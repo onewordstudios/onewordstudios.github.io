24d29859e0f82b3c4893c9c3635ba1ed7eefed24
mt-xing <mt-xing@users.noreply.github.com>
2020-05-20 17:25:12 -0400

    Scroll Effect

---
+++ sweetspace/scroll.js
@@ -0,0 +1,48 @@
+function setupScroll() {
+	const wrapEl = document.getElementById("scene");
+	const logoEl = document.getElementById("heroLogo");
+	const fadeEls = ["heroLogo", "studioName", "awardWinning"].map(x => document.getElementById(x));
+	function fade(o) {
+		fadeEls.forEach(x => x.style.opacity = o);
+	}
+	
+	/**
+	 * Whether a new animation repaint has already been requested
+	 * @type {boolean}
+	 */
+	let anim = false;
+	
+	let visible = true;
+	
+	console.log("Going");
+	
+	window.onscroll = function () {
+		console.log("Hi");
+		if (!anim) {
+			requestAnimationFrame(setScroll);
+		}
+		anim = true;
+	};
+	setScroll();
+	
+	function setScroll() {
+		const height = window.pageYOffset / window.innerHeight;
+		if (height <= 0.5) {
+			fade(1-(2*height));
+			visible = true;
+		}
+		if(height > 0.5 && height <= 1.5) {
+			fade(0);
+			wrapEl.style.transform = `translateY(${(height - 0.5)*100}%)`;
+			visible = true;
+		}
+		
+		if(visible && height > 1.5) {
+			visible = false;
+			fade(0);
+			wrapEl.style.transform = "translateY(100%)";
+		}
+		
+		anim = false;
+	}
+}
